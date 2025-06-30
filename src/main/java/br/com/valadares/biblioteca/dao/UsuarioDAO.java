fb028f5e342f55790e4f2e68dd36312be78eeaba
Gabriel Valadares <silva44gabriel@gmail.com>
2025-06-30 11:07:44 -0300

    feat: Adiciona UsuarioDAO e realiza testes de persistencia

---
+++ src/main/java/br/com/valadares/biblioteca/dao/UsuarioDAO.java
@@ -0,0 +1,25 @@
+package br.com.valadares.biblioteca.dao;
+
+import br.com.valadares.biblioteca.modelos.Usuario;
+import jakarta.persistence.EntityManager;
+
+import java.util.List;
+
+public class UsuarioDAO {
+    private EntityManager em;
+
+    public UsuarioDAO(EntityManager em) {
+        this.em = em;
+    }
+
+    public void cadastrar(Usuario usuario) {
+        em.persist(usuario);
+    }
+    public Usuario buscarUsuarioId(Long id) {
+        return this.em.find(Usuario.class, id);
+    }
+    public List<Usuario> buscarUsuarios() {
+        String jpql = "SELECT p FROM Usuario p";
+        return em.createQuery(jpql, Usuario.class).getResultList();
+    }
+}
--- src/main/java/br/com/valadares/biblioteca/principal/Main.java
+++ src/main/java/br/com/valadares/biblioteca/principal/Main.java
@@ -1,7 +1,9 @@
 package br.com.valadares.biblioteca.principal;
 
 import br.com.valadares.biblioteca.dao.LivroDAO;
+import br.com.valadares.biblioteca.dao.UsuarioDAO;
 import br.com.valadares.biblioteca.modelos.Livro;
+import br.com.valadares.biblioteca.modelos.Usuario;
 import br.com.valadares.biblioteca.util.JPAUtil;
 
 import jakarta.persistence.EntityManager;
@@ -11,12 +13,21 @@ public class Main {
         EntityManager em = JPAUtil.getEntityManager();
         Livro livro = new Livro("A chave da virada", "Marcelo nier",
                 "123123123", 10);
+        Usuario usuario = new Usuario("Gabriel", "email@gmail.com", "996965910");
+
         LivroDAO livroDAO = new LivroDAO(em);
+        UsuarioDAO usuarioDAO = new UsuarioDAO(em);
 
         em.getTransaction().begin();
+
         livroDAO.cadastrar(livro);
         livroDAO.buscarTodos();
+
+        usuarioDAO.cadastrar(usuario);
+        usuarioDAO.buscarUsuarios();
+
         em.getTransaction().commit();
+
         em.close();
     }
 
